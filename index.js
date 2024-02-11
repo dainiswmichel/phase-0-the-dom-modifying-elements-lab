b263fbc99c9d8b9088b1bed029e6d493c9c25ce0
Dainis W. Michel <dainis@dainis.net>
2024-02-12 00:52:13 +0100

    Creating and Inserting DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+document.querySelector('main#main').remove();
+
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Dainis is the champion";
+
+// Append the newHeader to the body or another suitable element
+document.body.appendChild(newHeader);
+
